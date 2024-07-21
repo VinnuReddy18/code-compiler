f61c6086e6d730359fdaa10a745a11a399b01643
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2024-07-21 12:08:10 +0530

    implemented server

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = ({ userLang, setUserLang, userTheme,
     ]
     return (
         <div className="navbar">
-            <h1>Geeks Code Compiler</h1>
+            <h1>Geek CodeDamn Compiler</h1>
             <Select options={languages} value={userLang}
                 onChange={(e) => setUserLang(e.value)}
                 placeholder={userLang} />
